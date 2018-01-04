7a901d40051169e404d4cfb8acb0145937fa106d
Bruno Correia <brunocs@msn.com>
2018-01-04 21:02:20 -0200

    Exercise 1 done.

---
+++ 01 Character Input/ex01.py
@@ -0,0 +1,5 @@
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+copies = int(input("How many copies do you want to see? "))
+year = str(100 - age + 2018)
+print(copies * ("Your name is " + name + " and you will turn 100 years old in " + year + ".\n"))
