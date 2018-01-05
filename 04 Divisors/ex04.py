894db8b5d09d8cabd21277dea1d8dd68214a1b9b
Bruno Correia <brunocs@msn.com>
2018-01-05 04:58:11 -0200

    Exercise 4 done.

--- 04 Divisors/ex04.py
+++ 04 Divisors/ex04.py
@@ -6,4 +6,4 @@
 number = int(input("Input a number: "))
 checkList = range(1, number+1)
 
-print([check for check in checkList if number % check == 0])
+print("Divisors: " + [check for check in checkList if number % check == 0])
