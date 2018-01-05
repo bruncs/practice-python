f64892c2ffee51606c8bcc90265bba58faafa24d
Bruno Correia <brunocs@msn.com>
2018-01-05 04:56:19 -0200

    Second argument of range corrected.

--- 04 Divisors/ex04.py
+++ 04 Divisors/ex04.py
@@ -4,6 +4,6 @@
 # pylint: disable=invalid-name
 
 number = int(input("Input a number: "))
-checkList = range(1, number)
+checkList = range(1, number+1)
 
 print([check for check in checkList if number % check == 0])
