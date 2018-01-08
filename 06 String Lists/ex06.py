736b0b863885998f6639c7cebacac67f5d1ce27b
Bruno Correia <brunocs@msn.com>
2018-01-08 16:45:55 -0200

    Exercise 6 done.

--- 06 String Lists/ex06.py
+++ 06 String Lists/ex06.py
@@ -3,8 +3,8 @@
 """
 # pylint: disable=invalid-name
 
-str = input("Input a string: ")
-if str[::-1] == str:
-  print("This is a fucking palindrome!")
+a = input("Input a string: ")
+if a[::-1] == a:
+    print("This is a fucking palindrome!")
 else:
-  print("This is ain't shit.")
+    print("This is ain't shit.")
