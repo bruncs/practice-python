21fbd3cd2d64376cf6cb4ea7c9b304ffdcc720a8
Bruno Correia <brunocs@msn.com>
2018-01-08 18:53:06 -0200

    Exercise 9 done.

--- 09 Guessing Game One/ex09.py
+++ 09 Guessing Game One/ex09.py
@@ -4,11 +4,11 @@
 # pylint: disable=invalid-name
 
 import random
-tries = 0
 playAgain = "y"
 
 while playAgain == "y":
-    guess = int(input("Guess a number from 1 to 9: "))
+    tries = 0
+    guess = int(input("\nGuess a number from 1 to 9: "))
     tries += 1
     number = random.randrange(1, 10)
 
@@ -20,7 +20,7 @@ while playAgain == "y":
             guess = int(input("Too high. Try again: "))
             tries += 1
 
-    print("You guessed the number "+ str(number) +" with "+ str(tries) +" tries.")
+    print("\nYou guessed the number "+ str(number) +" with "+ str(tries) +" tries.")
     playAgain = input("Would you like to play again (y/n)? ")
 
-print("Thank you for playing!")
+print("\nThank you for playing!")
